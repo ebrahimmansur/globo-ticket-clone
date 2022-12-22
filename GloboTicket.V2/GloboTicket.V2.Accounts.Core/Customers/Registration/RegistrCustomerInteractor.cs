85504fe5c101ed8881cd091ef5d1a22b25eb89ef
Ebrahim Mansur <ebrahimmansur90117@gmail.com>
2022-12-23 00:19:04 +0200

    add: customer registration interactor

---
+++ GloboTicket.V2/GloboTicket.V2.Accounts.Core/Customers/Registration/RegistrCustomerInteractor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.V2.Accounts.Core.Customers.Registration
+{
+    public class RegistrCustomerInteractor
+    {
+        void Execute(
+            string firstName,
+            string lastName,
+            string email,
+            string password
+            )
+        {
+            //proces business rules
+            // process command
+            // publish event
+        
+        }
+    }
+}
